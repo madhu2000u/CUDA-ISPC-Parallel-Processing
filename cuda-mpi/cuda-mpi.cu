d87f111b0a71fed5d7d6ac84690d8cb3b4c30561
Madhu Sudhanan <madhu2000u@gmail.com>
2023-10-25 17:19:33 -0400

    MPI-CUDA-2G-wSM v1.0 - Quad GPU tiled matrix mult and find 2 min on two workstations each with two GPUs using MPI added

---
+++ cuda-mpi/cuda-mpi.cu
@@ -0,0 +1,321 @@
+#include <iostream>
+#include <sys/time.h>
+
+#define MATRIX_SIZE 4096
+#define BLOCK_DIM 32                
+#define TILE_SZE BLOCK_DIM          //Tile size is same as block dimension. defined for better code understandability
+
+#define CHECK(call)\
+{\
+    const cudaError_t error = call;\
+    if(error != cudaSuccess)\
+    {\
+        std::cout<<"Error: "<<__FILE__<<":"<<__LINE__<<std::endl;\
+        std::cout<<"Code: "<<error<<", reason: "<<cudaGetErrorString(error)<<std::endl;\
+        exit(1);\
+    }\
+}
+
+typedef struct
+{
+    float value;
+    int16_t row, col;
+
+} matElement;
+
+//TODO remove
+// void matrixInit(float *a, float *b, float *c)
+// {
+//     for (int i = 0; i < MATRIX_SIZE; i++)
+//     {
+//         for (int j = 0; j < MATRIX_SIZE; j++)
+//         {   
+//             a[i * MATRIX_SIZE + j] = rand() / (float)1147654321;
+//             b[i * MATRIX_SIZE + j] = rand() / (float)1147654321;
+//             c[i * MATRIX_SIZE + j] = (float)0;
+//         }
+        
+//     }
+// }
+
+__device__ void reduceLastWarp(volatile matElement *newSharedB, int threadId)
+{
+    if(newSharedB[threadId].value > newSharedB[threadId + 32].value){
+        newSharedB[threadId].value = newSharedB[threadId + 32].value;
+        newSharedB[threadId].row = newSharedB[threadId + 32].row;
+        newSharedB[threadId].col = newSharedB[threadId + 32].col;
+    }
+
+    if(newSharedB[threadId].value > newSharedB[threadId + 16].value){
+        newSharedB[threadId].value = newSharedB[threadId + 16].value;
+        newSharedB[threadId].row = newSharedB[threadId + 16].row;
+        newSharedB[threadId].col = newSharedB[threadId + 16].col;
+    }
+
+    if(newSharedB[threadId].value > newSharedB[threadId + 8].value){
+        newSharedB[threadId].value = newSharedB[threadId + 8].value;
+        newSharedB[threadId].row = newSharedB[threadId + 8].row;
+        newSharedB[threadId].col = newSharedB[threadId + 8].col;
+    }
+
+    if(newSharedB[threadId].value > newSharedB[threadId + 4].value){
+        newSharedB[threadId].value = newSharedB[threadId + 4].value;
+        newSharedB[threadId].row = newSharedB[threadId + 4].row;
+        newSharedB[threadId].col = newSharedB[threadId + 4].col;
+    }
+
+    if(newSharedB[threadId].value > newSharedB[threadId + 2].value){
+        newSharedB[threadId].value = newSharedB[threadId + 2].value;
+        newSharedB[threadId].row = newSharedB[threadId + 2].row;
+        newSharedB[threadId].col = newSharedB[threadId + 2].col;
+    }
+
+    if(newSharedB[threadId].value > newSharedB[threadId + 1].value){
+        newSharedB[threadId].value = newSharedB[threadId + 1].value;
+        newSharedB[threadId].row = newSharedB[threadId + 1].row;
+        newSharedB[threadId].col = newSharedB[threadId + 1].col;
+    }
+}
+
+__device__ void minBlockReduce(matElement *newSharedB, int threadId)
+{
+    for (unsigned int stride = (BLOCK_DIM * BLOCK_DIM)/2; stride > 32; stride >>= 1)
+    {
+        if(threadId < stride)
+        {
+            if(newSharedB[threadId].value > newSharedB[threadId + stride].value){
+                newSharedB[threadId] = newSharedB[threadId + stride];
+            }
+        }
+        __syncthreads();
+    }
+    if(threadId < 32) reduceLastWarp(newSharedB, threadId);
+}
+
+__global__ void find2Min(int16_t firstMinRow, int16_t firstMinCol, float *c, matElement *d_minValueFromEachBlock)
+{
+    int16_t row = blockIdx.y * blockDim.y + threadIdx.y;
+    int16_t col = blockIdx.x * blockDim.x + threadIdx.x;
+
+    int16_t threadId = threadIdx.y * BLOCK_DIM + threadIdx.x;
+
+    __shared__ matElement sharedC[BLOCK_DIM * BLOCK_DIM];
+
+    if(row == 0 && col == 0) c[firstMinRow * MATRIX_SIZE + firstMinCol] = __FLT_MAX__;
+    __syncthreads();
+
+    sharedC[threadId].value = c[row * MATRIX_SIZE + col];
+    sharedC[threadId].row = row;
+    sharedC[threadId].col = col;
+    __syncthreads();
+
+    minBlockReduce(sharedC, threadId);
+    if(threadId == 0){   
+        d_minValueFromEachBlock[blockIdx.y * gridDim.x + blockIdx.x].value = sharedC[0].value;
+        d_minValueFromEachBlock[blockIdx.y * gridDim.x + blockIdx.x].row = sharedC[0].row;
+        d_minValueFromEachBlock[blockIdx.y * gridDim.x + blockIdx.x].col = sharedC[0].col;
+    }
+}
+
+__global__ void tiledMatrixMultiply(int gpu, float *a, float *b, float *c, matElement *d_minValueFromEachBlock)
+{
+    int16_t row = blockIdx.y * blockDim.y + threadIdx.y;
+    int16_t col = blockIdx.x * blockDim.x + threadIdx.x;
+
+    int16_t threadId = threadIdx.y * BLOCK_DIM + threadIdx.x;
+
+    __shared__ float sharedA[BLOCK_DIM * BLOCK_DIM];
+    __shared__ float sharedB[BLOCK_DIM * BLOCK_DIM * sizeof(matElement)];                    
+
+    float temp = 0;
+
+    for (int i = 0; i < gridDim.x; i++)
+    {
+        sharedA[threadId] = a[row * MATRIX_SIZE + (i * TILE_SZE + threadIdx.x)];                 //index into the global a with the global row (since we are tiling across x dimention of a) and each thread's tile 
+        sharedB[threadId] = b[(i * TILE_SZE + threadIdx.y) * MATRIX_SIZE + col];                 //index into the global b with each thread's tile idexes (since we are tiling across y dimention of b) and globale column 
+        __syncthreads();                                                                         //make sure all values of the sub-matrices are loaded by thre threads before proceding
+
+        for (int j = 0; j < TILE_SZE; j++)
+        {
+            temp += sharedA[threadIdx.y * TILE_SZE + j] * sharedB[j * TILE_SZE + threadIdx.x];
+        }
+
+        __syncthreads();                                                                         //make sure all sub-matrix calculation is done by threads before advancing to the next sub-matricies
+
+    }
+    matElement *newSharedB = (matElement*) sharedB;                                              //reuse shared mem for finding min element
+
+    newSharedB[threadId].value = temp;
+    newSharedB[threadId].row = row;
+    newSharedB[threadId].col = col;
+    __syncthreads();
+
+    c[row * 4096 + col] = temp;
+
+    minBlockReduce(newSharedB, threadId);
+    if(threadId == 0){   
+        d_minValueFromEachBlock[blockIdx.y * gridDim.x + blockIdx.x].value = newSharedB[0].value;
+        d_minValueFromEachBlock[blockIdx.y * gridDim.x + blockIdx.x].row = newSharedB[0].row;
+        d_minValueFromEachBlock[blockIdx.y * gridDim.x + blockIdx.x].col = newSharedB[0].col;
+    }
+
+}
+
+extern matElement* runCuda(int mpi_rank, int rowSizePerWorkstation, float *h_a, float *h_b)
+{   
+    int num_gpus;
+    cudaGetDeviceCount(&num_gpus);
+    int rowSizePerGpu = ((rowSizePerWorkstation - 1) / num_gpus) + 1;
+
+    struct timeval start_time, end_time;
+    double exec_time;
+    matElement minElement[num_gpus * 2];
+
+    for (int i = 0; i < num_gpus * 2; i++)
+    {
+        minElement[i].value = __FLT_MAX__;
+    }
+
+    // float *h_a, *h_b, *h_c; //TODO remove
+    float *d_a[num_gpus], *d_b, *d_c[num_gpus];             //d_b & h_b same for both gpus
+
+    cudaStream_t streams[num_gpus];
+
+    matElement *h_minValueFromEachBlock[num_gpus];
+    matElement *d_minValueFromEachBlock[num_gpus];
+
+    size_t size = MATRIX_SIZE * MATRIX_SIZE * sizeof(float);
+
+    int gridSize_y = rowSizePerGpu / BLOCK_DIM;
+    int gridSize_x = MATRIX_SIZE / BLOCK_DIM;
+
+    // //TODO remove
+    // h_a = (float*)malloc(size);
+    // h_b = (float*)malloc(size);
+    // h_c = (float*)malloc(size);
+    
+    for (int i = 0; i < num_gpus; i++)
+    {
+        cudaSetDevice(i);
+
+        h_minValueFromEachBlock[i] = (matElement*)malloc((gridSize_x) * (gridSize_y) * sizeof(matElement));      //gridSize_x because there are gridSize_x block across x axis and gridSize_x blocks * 32 block size = 4096 columns across x. similarly gridSize_y (here 64) blocks along y axis * 32 block size = 2048 rows per gpu
+
+        CHECK(cudaMallocHost(&d_a[i], rowSizePerGpu * MATRIX_SIZE * sizeof(float)));                    //each gpu gets rowSizePerGpu * MATRIX_SIZE (mxn) matrix for a
+        CHECK(cudaMallocHost(&d_b, size));                                                              //entire b matrix (nxn) is required for matrix multipliaion -_-
+        CHECK(cudaMallocHost(&d_c[i], rowSizePerGpu * MATRIX_SIZE * sizeof(float)));                    //resulting matrix per gpu is (mxn)
+        CHECK(cudaMallocHost(&d_minValueFromEachBlock[i], (gridSize_x) * (gridSize_y) * sizeof(matElement)));
+
+        cudaStreamCreate(&streams[i]);
+    }
+
+    // matrixInit(h_a, h_b, h_c);
+    
+    dim3 blockPerGrid(gridSize_x , gridSize_y);
+    dim3 threadsPerBlock(BLOCK_DIM, BLOCK_DIM);
+
+    
+    gettimeofday(&start_time, NULL);
+    for(int i = 0; i < num_gpus; i++)
+    {
+        cudaSetDevice(i);
+        CHECK(cudaMemcpyAsync(d_a[i], h_a + i * rowSizePerGpu * MATRIX_SIZE, rowSizePerGpu * MATRIX_SIZE * sizeof(float), cudaMemcpyHostToDevice, streams[i]));
+        CHECK(cudaMemcpyAsync(d_b, h_b, size, cudaMemcpyHostToDevice, streams[i]));
+        
+        tiledMatrixMultiply<<<blockPerGrid, threadsPerBlock, 0, streams[i]>>>(i, d_a[i], d_b, d_c[i], d_minValueFromEachBlock[i]);
+
+        // /CHECK(cudaMemcpyAsync(h_c + i * rowSizePerGpu * MATRIX_SIZE, d_c[i], rowSizePerGpu * MATRIX_SIZE * sizeof(float), cudaMemcpyDeviceToHost, streams[i]));
+        CHECK(cudaMemcpyAsync(h_minValueFromEachBlock[i], d_minValueFromEachBlock[i], (gridSize_x) * (gridSize_y) * sizeof(matElement), cudaMemcpyDeviceToHost, streams[i]));
+    }
+
+    for (int i = 0; i < num_gpus; i++)
+    {
+        cudaSetDevice(i);
+        cudaStreamSynchronize(streams[i]);
+    }
+    
+    for (int j = 0; j < num_gpus; j++)
+    {
+        for (int i = 0; i < (gridSize_x) * (gridSize_y); i++)
+        {
+            if(h_minValueFromEachBlock[j][i].value < minElement[j * num_gpus].value)
+            {   
+                minElement[j * num_gpus].value = h_minValueFromEachBlock[j][i].value;           
+                minElement[j * num_gpus].row = h_minValueFromEachBlock[j][i].row + ((mpi_rank == 0 ? j : (j + 2)) * rowSizePerGpu);         //each gpu uses it's own row, col addressing from 0. but gpu 1 maybe hadling rows from 2048 which it considers as rows from 0 and finds its min value index so we scale it for each gpu here
+                minElement[j * num_gpus].col = h_minValueFromEachBlock[j][i].col;
+            }
+        }
+    }
+
+    for(int j = 0; j < num_gpus; j++)
+    {   
+        cudaSetDevice(j);
+        find2Min<<<blockPerGrid, threadsPerBlock, 0, streams[j]>>>((minElement[j * num_gpus].row - ((mpi_rank == 0 ? j : (j + 2)) * rowSizePerGpu)), minElement[j * num_gpus].col, d_c[j], d_minValueFromEachBlock[j]);
+
+        CHECK(cudaMemcpyAsync(h_minValueFromEachBlock[j], d_minValueFromEachBlock[j], (gridSize_x) * (gridSize_y) * sizeof(matElement), cudaMemcpyDeviceToHost, streams[j]));
+    }
+
+    for (int i = 0; i < num_gpus; i++)
+    {
+        cudaSetDevice(i);
+        cudaStreamSynchronize(streams[i]);
+    }
+
+    for (int j = 0; j < num_gpus; j++)
+    {    
+        for (int i = 0; i < (gridSize_x) * (gridSize_y); i++)
+        {
+            if(h_minValueFromEachBlock[j][i].value < minElement[j * num_gpus + 1].value)
+            {
+                minElement[j * num_gpus + 1].value = h_minValueFromEachBlock[j][i].value;           
+                minElement[j * num_gpus + 1].row = h_minValueFromEachBlock[j][i].row + ((mpi_rank == 0 ? j : (j + 2)) * rowSizePerGpu);
+                minElement[j * num_gpus + 1].col = h_minValueFromEachBlock[j][i].col;
+            }
+        }
+    }
+    
+    static matElement finalMinElements[2];
+    float minVal = __FLT_MAX__;
+    int minIndex = 0;
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < num_gpus * 2; j++)
+        {
+            if(minElement[j].value < minVal)
+            {
+                minVal = minElement[j].value;
+                minIndex = j;
+            }
+        }
+        finalMinElements[i] = minElement[minIndex];
+        minVal = __FLT_MAX__;
+        minElement[minIndex].value = __FLT_MAX__;
+    }
+    
+    gettimeofday(&end_time, NULL);
+
+    //Free device memory
+    for (int i = 0; i < num_gpus; i++)
+    {
+        cudaFree(d_a[i]);
+        cudaFree(d_b);
+        cudaFree(d_c[i]);
+        cudaFree(d_minValueFromEachBlock[i]);
+    } 
+
+    cudaDeviceProp prop1;
+    cudaGetDeviceProperties(&prop1, 0);
+    cudaDeviceProp prop2;
+    cudaGetDeviceProperties(&prop2, 0);
+    exec_time = (double)(end_time.tv_sec - start_time.tv_sec) + (double)(end_time.tv_usec - start_time.tv_usec)/(double)1000000;
+
+    std::cout<<"Execution time - "<<exec_time<<std::endl;
+    
+    std::cout<<"Matrix size - "<<MATRIX_SIZE<<std::endl;
+
+    std::cout<<"Printing from rank "<<mpi_rank<<" GPU - "<<prop1.name<<" "<<prop1.major<<"."<<prop1.minor<<", min value 1 (val, row, col) - ("<<finalMinElements[0].value<<", "<<finalMinElements[0].row<<", "<<finalMinElements[0].col<<") "<<std::endl;
+
+    std::cout<<"Printing from rank "<<mpi_rank<<" GPU - "<<prop2.name<<" "<<prop2.major<<"."<<prop2.minor<<", min value 2 (val, row, col) - ("<<finalMinElements[1].value<<", "<<finalMinElements[1].row<<", "<<finalMinElements[1].col<<") "<<std::endl;
+
+    return finalMinElements;
+
+}
---
+++ cuda-mpi/main-mpi.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+#include <mpi.h>
+#include <sys/time.h>
+
+#define MATRIX_SIZE 4096
+#define NoW 2                                   //Network of Workstations
+
+#define MCHECK(call) \
+{ int result; \
+    result = call; \
+    if(result != MPI_SUCCESS) \
+    {\
+        fprintf(stderr, "Call "#call" returned error code %i \n", result); \
+        MPI_Abort(MPI_COMM_WORLD, result); \
+    }\
+}
+
+typedef struct
+{
+    float value;
+    int16_t row, col;
+
+} matElement; 
+
+matElement* runCuda(int, int, float*, float*);
+
+void matrixInit1(float *a, float *b)
+{
+    for (int i = 0; i < MATRIX_SIZE; i++)
+    {
+        for (int j = 0; j < MATRIX_SIZE; j++)
+        {   
+            a[i * MATRIX_SIZE + j] = rand() / (float)1147654321;
+            b[i * MATRIX_SIZE + j] = rand() / (float)1147654321;
+            // c[i * MATRIX_SIZE + j] = (float)0;
+        }
+        
+    }
+}
+
+
+int main(int argc, char* argv[])
+{   
+    struct timeval start_time, end_time;
+    double exec_time;
+    MPI_Status recvStatus;
+    int rank;
+    int tag = 100;
+    int rowSizePerWorkstation = MATRIX_SIZE / NoW;
+
+    float *h_a, *h_b;
+
+    float *h_a_r1, *h_b_r1;
+
+    size_t size = MATRIX_SIZE * MATRIX_SIZE * sizeof(float);
+
+    size_t mpiBuffSize = (size / 2) / sizeof(float);            //per workstation
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    MPI_Datatype MPI_matElement;
+    MPI_Datatype type[3] = {MPI_FLOAT, MPI_INT, MPI_INT};
+    int blocklen[3] = {1, 1, 1};
+    MPI_Aint disp[3];
+    disp[0] = 0;
+    disp[1] = sizeof(int16_t);
+    disp[2] = sizeof(int16_t) + sizeof(int16_t);
+
+    MPI_Type_create_struct(3, blocklen, disp, type, &MPI_matElement);
+    MPI_Type_commit(&MPI_matElement);
+    
+    if(rank == 0)   //main
+    {   
+        matElement *minElementsFromNoW[NoW];
+        matElement final[2];
+        h_a = (float*)malloc(size);
+        h_b = (float*)malloc(size);
+        
+        minElementsFromNoW[1] = (matElement*) malloc(2 * sizeof(matElement));
+        // final = (matElement*) malloc(2 * sizeof(matElement));
+        // minElementsFromNoW1[0].value = 10;
+        // minElementsFromNoW1[1]->value = 20;
+        // *minElementsFromNoW = (matElement*)malloc(NoW * sizeof(matElement));
+
+        matrixInit1(h_a, h_b);
+        gettimeofday(&start_time, NULL);
+        MCHECK(MPI_Send(h_a + mpiBuffSize , mpiBuffSize, MPI_FLOAT, 1, tag, MPI_COMM_WORLD));
+        MCHECK(MPI_Send(h_b , (size / sizeof(float)), MPI_FLOAT, 1, tag, MPI_COMM_WORLD));              //no need to multiply by sizeof(float) as mpi_send implicitly determines it and if you do it will give seg fault
+        printf("sending done\n");
+        minElementsFromNoW[0] = runCuda(rank, rowSizePerWorkstation, h_a, h_b);
+        printf("waiting to recienve min elements\n");
+        MCHECK(MPI_Recv(minElementsFromNoW[1], 2, MPI_matElement, 1, tag, MPI_COMM_WORLD, &recvStatus));
+        printf("recieved min elements\n");
+        printf("rank 0 %d\n", (minElementsFromNoW[1]+1)->row);
+
+        float minVal = __FLT_MAX__;
+        int minIndex_j = 0;
+        int minIndex_k = 0;
+        for (int i = 0; i < NoW; i++)
+        {
+            for (int j = 0; j < NoW; j++)
+            {
+                for(int k = 0; k < 2; k++)
+                {    
+                    if((minElementsFromNoW[j] + k)->value < minVal)
+                    {
+                        minVal = (minElementsFromNoW[j] + k)->value;
+                        minIndex_j = j;
+                        minIndex_k = k;
+                    }
+                }
+            }
+
+            final[i] = *(minElementsFromNoW[minIndex_j] + minIndex_k);
+            minVal = __FLT_MAX__;
+            (minElementsFromNoW[minIndex_j] + minIndex_k)->value = __FLT_MAX__;
+        }
+        gettimeofday(&end_time, NULL);
+        
+        exec_time = (double)(end_time.tv_sec - start_time.tv_sec) + (double)(end_time.tv_usec - start_time.tv_usec)/(double)1000000;
+        std::cout<<"Execution time by host of rank 0 - "<<exec_time<<std::endl;
+        std::cout<<"Final min value 1 (val, row, col) - ("<<final[0].value<<", "<<final[0].row<<", "<<final[0].col<<"), "<<"printing from rank "<<rank<<std::endl;
+
+        std::cout<<"Final min value 2 (val, row, col) - ("<<final[1].value<<", "<<final[1].row<<", "<<final[1].col<<"), "<<"printing from rank "<<rank<<std::endl;
+
+    }
+    else if(rank == 1)
+    {   
+        matElement *minElementsFromNoW;
+        printf("i am rank 1\n");
+        h_a_r1 = (float*)malloc(rowSizePerWorkstation * MATRIX_SIZE * sizeof(float));
+        h_b_r1 = (float*)malloc(size);
+        MCHECK (MPI_Recv(h_a_r1, mpiBuffSize, MPI_FLOAT, 0, tag, MPI_COMM_WORLD, &recvStatus));
+        MCHECK (MPI_Recv(h_b_r1, (size / sizeof(float)), MPI_FLOAT, 0, tag, MPI_COMM_WORLD, &recvStatus));
+        printf("sending recved\n");
+        minElementsFromNoW = runCuda(rank, rowSizePerWorkstation, h_a_r1, h_b_r1);
+        printf("size of minElementsFromNoW %lu\n", sizeof(matElement));
+        MCHECK(MPI_Send(minElementsFromNoW, 2, MPI_matElement, 0, tag, MPI_COMM_WORLD)); 
+        printf("rank 1 %f\n", minElementsFromNoW[1].value);
+    }
+
+    MPI_Finalize();
+    
+}
